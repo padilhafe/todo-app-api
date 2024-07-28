18edffa872bf3a308e2c646d42d4cc1dcb394d1f
padilhafe <62897335+padilhafe@users.noreply.github.com>
2024-07-28 13:49:31 -0300

    feat: mysql database support

--- database.py
+++ database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todos_app.sqlite"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://todo_user:todo_password@localhost:3306/todos"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
